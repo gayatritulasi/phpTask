2504663dccca9f751820f6e573d085b593c69c81
gayatritulasi <tulasi.gayathri@gmail.com>
2021-12-07 16:49:07 +0530

    Update connection.php

--- task/connection.php
+++ task/connection.php
@@ -1,11 +1,11 @@
 <?php
      
-     $servername = "localhost";
-     $username = "root";
-     $password = "";
+     $servername = "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+     $username = "y1wg4kylnfmlib7h";
+     $password = "xfvj98yg2braoyww";
  
  try {
-   $conn = new PDO("mysql:host=$servername;dbname=task", $username, $password);
+   $conn = new PDO("mysql:host=$servername;dbname=lu0lb5uqa9ske47p", $username, $password);
    // set the PDO error mode to exception
    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    
