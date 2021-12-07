a7c41e823cf063a0b7867deb822f2e976662e2c9
gayatritulasi <tulasi.gayathri@gmail.com>
2021-12-07 17:43:42 +0530

    Update contact_mail1.php

--- task/contact_mail1.php
+++ task/contact_mail1.php
@@ -6,6 +6,7 @@ include ('phpmailer/Exception.php');
 
 
 // The message
+try{
 $message = 'Iam '.$_GET['firstname']. ' '.$_GET['lastname'].'from ' .$_GET['country'].' would like to update you about! '.$_GET['subject'] ;
 $subject = 'Contact - Movies';
 $to = 'tulasi.gayathri@gmail.com';
@@ -26,6 +27,9 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
         echo 'Could not send mail';
     }
  
- 
+}
+catch(Exception $e) {
+  echo 'Message: ' .$e->getMessage();
+}
 
 ?>
